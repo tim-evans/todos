b0a261482897a998bcc7af8c791978b8279650f5
Tim Evans <tim-evans@sproutcore.com>
2012-02-24 15:16:18 -0500

    only create todos if they have a value and add timestamps to todo items

--- apps/todos/controllers/todo.js
+++ apps/todos/controllers/todo.js
@@ -3,9 +3,13 @@ require('core');
 // A controller that will provide a list of all of the todos.
 Todos.todoController = SC.ArrayController.create({
   addTodo: function (view) {
-    Todos.store.createRecord(Todos.Todo, {
-      title: view.get('value')
-    });
-    view.set('value', '');
+    var todo = (view.get('value') || '').trim();
+    if (todo !== '') {
+      Todos.store.createRecord(Todos.Todo, {
+        title: todo,
+        timestamp: SC.DateTime.create()
+      });
+      view.set('value', '');
+    }
   }
 });
