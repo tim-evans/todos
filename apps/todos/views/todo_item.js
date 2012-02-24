ec82579dd667dd020742557aa9c87860defcc2be
Tim Evans <tim-evans@sproutcore.com>
2012-02-24 11:47:10 -0500

    add todo item view

---
+++ apps/todos/views/todo_item.js
@@ -0,0 +1,17 @@
+Todo.TodoItemView = SC.View.extend({
+
+  childViews: ['todo', 'checkbox'],
+
+  // The label of the todo item
+  todo: SC.LabelView.design({
+    contentBinding: SC.Binding.oneWay('.parentView.content'),
+    layout: { left: 47 }
+  }),
+
+  // The checkbox that shows whether this todo item is complete
+  checkbox: SC.CheckboxView.design({
+    layout: { zIndex: 2 },
+    valueBinding: '.parentView.content.isCompleted'
+  })
+
+});
