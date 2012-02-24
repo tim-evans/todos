d9e0d8db298a6473f7173b20a4ae4a99f279a489
Tim Evans <tim-evans@sproutcore.com>
2012-02-24 12:34:07 -0500

    style header as a toolbar view

--- apps/todos/resources/main_page.js
+++ apps/todos/resources/main_page.js
@@ -12,16 +12,27 @@ Todos.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
   mainPane: SC.MainPane.design({
-    childViews: ['completeAll', 'todosList'],
+    childViews: ['header', 'todosList'],
 
-    completeAll: SC.CheckboxView.design({
-      layout: { centerX: 0, width: 400, top: 0, height: 30 },
-      title: 'Mark All as done',
-      valueBinding: 'Todos.completedTodosController.areAllCompleted'
+    header: SC.ToolbarView.design({
+      layout: { centerX: 0, width: 500, top: 0, height: 36 },
+
+      childViews: ['title', 'completeAll'],
+
+      completeAll: SC.CheckboxView.design(SC.AutoResize, {
+        autoResizePadding: { width: 47 },
+        title: 'Mark All as done',
+        valueBinding: 'Todos.completedTodosController.areAllCompleted'
+      }),
+
+      title: SC.LabelView.design({
+        layout: { left: 0, right: 0, top: 0, bottom: 0 },
+        value: 'Todos'
+      })
     }),
     
     todosList: SC.ListView.design({
-      layout: { centerX: 0, width: 400, top: 30, bottom: 0 },
+      layout: { centerX: 0, width: 500, top: 36, bottom: 0 },
       contentBinding: SC.Binding.oneWay('Todos.todoController'),
       rowHeight: 36,
       exampleView: Todos.TodoItemView
