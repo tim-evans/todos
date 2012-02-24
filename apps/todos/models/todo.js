ed47f5cd899f13afa64d352692883871c8bf4424
Tim Evans <tim-evans@sproutcore.com>
2012-02-24 11:52:35 -0500

    fix Todo model (it should be using extend rather than create)

--- apps/todos/models/todo.js
+++ apps/todos/models/todo.js
@@ -2,7 +2,7 @@ require('core');
 
 // A simple SproutCore model that describes
 // the semantics of something that needs to be done.
-Todos.Todo = SC.Record.create({
+Todos.Todo = SC.Record.extend({
 
   // Is the Todo item completed?
   isCompleted: SC.Record.attr(Boolean),
